c9b9094bfeb1a1f3eda9750a914b3f943927d57e
BosEriko <bos.eriko@gmail.com>
2020-11-15 17:51:37 +0800

    add JsonWebToken simpleton

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,9 @@ module Myapp
     # Initialize config/default.yml so you can use it
     config.default = config_for(:default)
 
+    # Load the lib folder
+    config.autoload_paths << Rails.root.join('lib')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
