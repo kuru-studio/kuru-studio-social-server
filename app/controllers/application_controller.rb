77105d84f550ca87912492e6f0949d61adef0684
BosEriko <bos.eriko@gmail.com>
2020-12-01 15:33:14 +0800

    move auth user to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  def authenticate_user!
+    verifier = FirebaseVerifier.new("kuru-studio-social-firebase")
+
+    valid_public_keys = FirebaseVerifier.retrieve_and_cache_jwt_valid_public_keys
+    kid = valid_public_keys.keys[0]
+    rsa_public = OpenSSL::X509::Certificate.new(valid_public_keys[kid]).public_key
+
+    decoded_token = verifier.decode(http_auth_header, rsa_public)
+
+    @firebase_data = {
+      :payload => decoded_token[0],
+      :headers => decoded_token[1]
+    }
+
+    render json: @firebase_data
+  end
+
+  def http_auth_header
+    if request.headers['Authorization'].present?
+      return request.headers['Authorization'].split(' ').last
+    else
+      render json: { :error => "Missing Token" }
+    end
+  end
 end
