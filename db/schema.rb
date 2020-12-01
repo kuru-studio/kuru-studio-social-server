f9368cb282a64c0b36119ab872e117c7dcef9e70
BosEriko <bos.eriko@gmail.com>
2020-12-01 15:14:47 +0800

    cleaup 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_045321) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "test_blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
