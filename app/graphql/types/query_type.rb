f6e03a248e1ea8aa19401e761956fd38185bda30
BosEriko <bos.eriko@gmail.com>
2021-03-12 15:44:57 +0800

    definte all_groups type inside query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    # queries are just represented as fields
+    # `all_groups` is automatically camelcased to `allGroups`
+    field :all_groups, [GroupType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # this method is invoked, when `all_group` fields is being resolved
+    def all_groups
+      Group.all
     end
   end
 end
