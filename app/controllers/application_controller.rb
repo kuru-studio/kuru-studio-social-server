b7e17e5818b01a15aacd1d835bb1376b50b0bfa0
BosEriko <bos.eriko@gmail.com>
2021-03-15 17:13:57 +0800

    make current_user and user_signed_in? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,15 @@
 class ApplicationController < ActionController::API
   def authenticate_user!
-    verifier = FirebaseVerifier.new(Rails.application.credentials.send(Rails.env)[:database][:firebase_project_id])
-
-    valid_public_keys = FirebaseVerifier.retrieve_and_cache_jwt_valid_public_keys
-    kid = valid_public_keys.keys[0]
-    rsa_public = OpenSSL::X509::Certificate.new(valid_public_keys[kid]).public_key
-
-    decoded_token = verifier.decode(http_auth_header, rsa_public)
-    firebase_login(decoded_token[0]["user_id"])
+    @current_user = user_authenticator
   end
 
   def user_signed_in?
-    # Should return a boolean indicating if a user is signed in or not
+    return false if @current_user.nil?
+    true
   end
 
   def current_user
-    # Should return an object containing the current user
+    @current_user
   end
 
   private
@@ -27,13 +21,24 @@ class ApplicationController < ActionController::API
       end
     end
 
+    def user_authenticator
+      verifier = FirebaseVerifier.new(Rails.application.credentials.send(Rails.env)[:database][:firebase_project_id])
+
+      valid_public_keys = FirebaseVerifier.retrieve_and_cache_jwt_valid_public_keys
+      kid = valid_public_keys.keys[0]
+      rsa_public = OpenSSL::X509::Certificate.new(valid_public_keys[kid]).public_key
+
+      decoded_token = verifier.decode(http_auth_header, rsa_public)
+      firebase_login(decoded_token[0]["user_id"])
+    end
+
     def firebase_login(user_id)
-      @current_user = User.find_by_firebase_user_id(user_id)
-      if @current_user.nil?
-        @current_user = User.new
-        @current_user.firebase_user_id = user_id
-        @current_user.save
+      current_user = User.find_by_firebase_user_id(user_id)
+      if current_user.nil?
+        current_user = User.new
+        current_user.firebase_user_id = user_id
+        current_user.save
       end
-      @current_user
+      current_user
     end
 end
