a5c3c0f8bc447cce03051b8187a6f14ceb399198
BosEriko <bos.eriko@gmail.com>
2021-11-17 14:00:03 +0800

    remove user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,2 @@
 class User < ApplicationRecord
-  validates :firebase_user_id, presence: true, uniqueness: true
-
-  def find_by_firebase_user_id(user_id)
-    self.find_by(firebase_user_id: user_id)
-  end
 end
