786fcd58892624befbc360d678b8d681381c5620
BosEriko <bos.eriko@gmail.com>
2021-11-17 16:16:34 +0800

    add a way to authenticate user on graphql controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -9,8 +9,9 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      # we need to provide session and current user
+      session: session,
+      current_user: current_user
     }
     result = MyappSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -20,6 +21,19 @@ class GraphqlController < ApplicationController
   end
 
   private
+
+  # gets current user from token stored in the session
+  def current_user
+    # if we want to change the sign-in strategy, this is the place to do it
+    return unless session[:token]
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+    token = crypt.decrypt_and_verify session[:token]
+    user_id = token.gsub('user-id:', '').to_i
+    User.find user_id
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
 
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
