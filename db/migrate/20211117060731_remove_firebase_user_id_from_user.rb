47da1316f0cfff8ab1fdb00653ed975fecc783a5
BosEriko <bos.eriko@gmail.com>
2021-11-17 14:38:06 +0800

    update user model

--- db/migrate/20211117060731_remove_firebase_user_id_from_user.rb
+++ db/migrate/20211117060731_remove_firebase_user_id_from_user.rb
@@ -1,5 +1,5 @@
 class RemoveFirebaseUserIdFromUser < ActiveRecord::Migration[6.0]
   def change
-    # remove_column :users, :firebase_user_id, :string
+    remove_column :users, :firebase_user_id, :string
   end
 end
--- db/migrate/20211117061548_add_authentication_columns_to_user.rb
+++ db/migrate/20211117061548_add_authentication_columns_to_user.rb
@@ -1,7 +1,7 @@
 class AddAuthenticationColumnsToUser < ActiveRecord::Migration[6.0]
   def change
-    # add_column :users, :name, :string
-    # add_column :users, :email, :string
-    # add_column :users, :password_digest, :string
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :users, :password_digest, :string
   end
 end
