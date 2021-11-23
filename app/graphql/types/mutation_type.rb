600fe24c237d1f16756d39210429550b586b4d19
BosEriko <bos.eriko@gmail.com>
2021-11-23 10:47:18 +0800

    add mutation to mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
     field :create_user, mutation: Mutations::CreateUser
     field :create_group, mutation: Mutations::CreateGroup
     field :create_post, mutation: Mutations::CreatePost
+    field :create_like, mutation: Mutations::CreateLike
     field :signin_user, mutation: Mutations::SignInUser
   end
 end
