a3d1f0c0f60d417743ecff643e756301fb5471fd
BosEriko <bos.eriko@gmail.com>
2021-11-23 10:01:47 +0800

    rollback schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_012323) do
+ActiveRecord::Schema.define(version: 2021_11_17_081824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,6 @@ ActiveRecord::Schema.define(version: 2021_11_23_012323) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_groups_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
