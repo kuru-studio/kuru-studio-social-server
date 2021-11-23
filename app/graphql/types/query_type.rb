8cd33bb4f28e28bff9450f1280aeb0fec329c50a
BosEriko <bos.eriko@gmail.com>
2021-11-24 02:06:16 +0800

    add field to query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,8 @@ module Types
     field :all_groups, [GroupType], null: false
     field :all_posts, [PostType], null: false
 
+    field :search_posts, [PostType], resolver: Resolvers::PostsSearch
+
     def all_groups() Group.all end
     def all_posts() Post.all end
   end
