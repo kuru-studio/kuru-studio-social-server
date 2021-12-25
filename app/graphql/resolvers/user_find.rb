46a3b7f8331feba75dcbac684be675829fc454d8
BosEriko <bos.eriko@gmail.com>
2021-12-25 09:03:52 +0800

    add a way to fetch a single user

--- app/graphql/resolvers/user_find.rb
+++ app/graphql/resolvers/user_find.rb
@@ -1,6 +1,5 @@
 class Resolvers::UserFind < GraphQL::Schema::Resolver
-  type [Types::UserType], null: false
-
+  type Types::UserType, null: false
   argument :id, ID, required: true
 
   def resolve(id:)
