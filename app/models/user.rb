03b187ebda713980da2d482dc028d3b7e23b4871
BosEriko <bos.eriko@gmail.com>
2021-12-31 23:45:01 +0800

    add email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_many :likes
   has_many :posts
