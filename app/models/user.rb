b06651e0623fef1cf956f5aff0ddda40f4cad8b6
BosEriko <bos.eriko@gmail.com>
2022-01-02 05:10:22 +0800

    capitalize first_name and last_name before saving to the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }, length: {minimum: 3, maximum: 20}
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  before_save { first_name.capitalize! }
+  before_save { last_name.capitalize! }
+
   has_many :likes
   has_many :posts
 end
