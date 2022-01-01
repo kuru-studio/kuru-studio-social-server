d3a609b3a57d7d501c3bf94bb21889c04cb37f04
BosEriko <bos.eriko@gmail.com>
2022-01-02 05:10:08 +0800

    add types to the new rows on user on the type user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,6 +3,12 @@ module Types
     field :id, ID, null: false
     field :username, String, null: false
     field :email, String, null: false
+    field :birthday, GraphQL::Types::ISO8601Date, null: false
+    field :country, String, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :short_description, String, null: false
+    field :about, String, null: false
 
     field :posts, [PostType], null: false
     field :likes, [LikeType], null: false
