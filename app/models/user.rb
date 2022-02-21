36b1d926a40218b71ccf6be0d81137ed4f84a665
BosEriko <bos.eriko@gmail.com>
2022-02-21 22:10:30 +0800

    update user model to accomodate invitation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :invitations
+  has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation', foreign_key: "friend_id"
 
   validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }, length: {minimum: 3, maximum: 20}
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
@@ -9,4 +11,19 @@ class User < ApplicationRecord
 
   has_many :likes
   has_many :posts
+
+  def friends
+    friends_i_sent_invitation = Invitation.where(user_id: id, confirmed: true).pluck(:friend_id)
+    friends_i_got_invitation = Invitation.where(friend_id: id, confirmed: true).pluck(:user_id)
+    ids = friends_i_sent_invitation + friends_i_got_invitation
+    User.where(id: ids)
+  end
+
+  def friend_with?(user)
+    Invitation.confirmed_record?(id, user.id)
+  end
+
+  def send_invitation(user)
+    invitations.create(friend_id: user.id)
+  end
 end
