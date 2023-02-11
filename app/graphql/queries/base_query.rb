161cf1a5a1e1900ae423ed131282ca6f9c7a76aa
BosEriko <bos.eriko@gmail.com>
2023-02-11 20:22:01 +0800

    Feature: Add base_query.rb

---
+++ app/graphql/queries/base_query.rb
@@ -0,0 +1,12 @@
+module Queries
+  # Base Queries class
+  class BaseQuery < GraphQL::Schema::Resolver
+    private
+
+    def check_authentication!
+      # return if context[:current_user]
+
+      # raise GraphQL::ExecutionError, 'You need to authenticate to perform this action'
+    end
+  end
+end
--- app/graphql/queries/post/all.rb
+++ app/graphql/queries/post/all.rb
@@ -7,6 +7,8 @@ module Queries
       type [Types::PostType], null: false
 
       def resolve(limit: 20, offset: 0)
+        check_authentication!
+
         posts = ::Post.order('created_at DESC')
         posts.offset(offset).limit(limit)
       end
