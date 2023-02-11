f240e97fefe476c5287be6fb2c503f405f94b294
BosEriko <bos.eriko@gmail.com>
2023-02-11 21:37:59 +0800

    Feature: Create a simple seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Post.create(content: "This is the first post!")
+Post.create(content: "This is the second post!")
+Post.create(content: "This is the third post!")
