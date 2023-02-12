e3e11e39a628f34797918f7f6307174261e9f662
BosEriko <bos.eriko@gmail.com>
2023-02-12 10:30:33 +0800

    Feature: Add User data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Post.create(content: "This is the first post!")
-Post.create(content: "This is the second post!")
-Post.create(content: "This is the third post!")
+Post.create(content: "The world isn’t perfect. But it’s there for us, doing the best it can. That’s what makes it so damn beautiful. — Roy Mustang (Full Metal Alchemist)")
+Post.create(content: "I will always love you, no matter which what time, or where I am. I’ll say it one more time. I love you. — Okabe (Steins; Gate)")
+Post.create(content: "I’ll leave tomorrow’s problems to tomorrow’s me. – Saitama (One-Punch Man)")
+
+User.create(name: "Roy Mustang", email: "roy@fma.com", password: "bl@ck_hAyat3")
+User.create(name: "Rintaro Okabe", email: "okabe@gate.com", password: "kvrisv~tensh1")
+User.create(name: "Caped Baldy", email: "saitama@opm.com", password: "$up3r$@le")
