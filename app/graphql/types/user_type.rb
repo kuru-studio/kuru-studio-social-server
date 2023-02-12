807b18ef435e3d6027e1188fc674a7340b324da3
BosEriko <bos.eriko@gmail.com>
2023-02-12 10:01:50 +0800

    Feature: Create type for users

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :email, String, null: false
+  end
+end
