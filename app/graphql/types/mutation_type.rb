b367ae3760449f2b89bfd660711c0ff4b18c7bd8
BosEriko <bos.eriko@gmail.com>
2023-02-20 17:59:12 +0800

    Feature: Re-initialize endpoint for user sign in

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_post, mutation: Mutations::Post::Create
     field :create_user, mutation: Mutations::User::Create
+    field :sign_in, mutation: Mutations::User::SignIn
   end
 end
