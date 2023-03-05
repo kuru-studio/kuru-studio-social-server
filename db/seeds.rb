7f99bd925e218f158d337b8607fc7672c2e02a05
BosEriko <bos.eriko@gmail.com>
2023-03-05 22:37:44 +0800

    Feature: Add association between Posts and Users on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Post.create(content: "The world isn’t perfect. But it’s there for us, doing the best it can. That’s what makes it so damn beautiful. — Roy Mustang (Full Metal Alchemist)")
-Post.create(content: "I will always love you, no matter which what time, or where I am. I’ll say it one more time. I love you. — Okabe (Steins; Gate)")
-Post.create(content: "I’ll leave tomorrow’s problems to tomorrow’s me. – Saitama (One-Punch Man)")
+first_user = User.create(name: "Roy Mustang", email: "roy@fma.com", password: "bl@ck_hAyat3")
+second_user = User.create(name: "Rintaro Okabe", email: "okabe@gate.com", password: "kvrisv~tensh1")
+third_user = User.create(name: "Caped Baldy", email: "saitama@opm.com", password: "$up3r$@le")
 
-User.create(name: "Roy Mustang", email: "roy@fma.com", password: "bl@ck_hAyat3")
-User.create(name: "Rintaro Okabe", email: "okabe@gate.com", password: "kvrisv~tensh1")
-User.create(name: "Caped Baldy", email: "saitama@opm.com", password: "$up3r$@le")
+Post.create(
+  content: "The world isn’t perfect. But it’s there for us, doing the best it can. That’s what makes it so damn beautiful. — Roy Mustang (Full Metal Alchemist)",
+  user_id: first_user.id
+)
+Post.create(
+  content: "I will always love you, no matter which what time, or where I am. I’ll say it one more time. I love you. — Okabe (Steins; Gate)",
+  user_id: second_user.id
+)
+Post.create(
+  content: "I’ll leave tomorrow’s problems to tomorrow’s me. – Saitama (One-Punch Man)",
+  user_id: third_user.id
+)
