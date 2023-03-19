9b761d235926e6e988b7dc4eff71f54dfa779d08
BosEriko <bos.eriko@gmail.com>
2023-03-19 10:58:21 +0800

    Feature: Add authentication check on base_query

--- app/graphql/queries/base_query.rb
+++ app/graphql/queries/base_query.rb
@@ -4,9 +4,9 @@ module Queries
     private
 
     def check_authentication!
-      # return if context[:current_user]
+      return if context[:current_user]
 
-      # raise GraphQL::ExecutionError, 'You need to authenticate to perform this action'
+      raise GraphQL::ExecutionError, 'You need to authenticate to perform this action'
     end
   end
 end
