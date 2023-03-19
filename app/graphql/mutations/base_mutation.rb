314e5be7f9e932f9d2df363397036859cdf8a6d4
BosEriko <bos.eriko@gmail.com>
2023-03-19 21:08:01 +0800

    Hotfix: Remove duplicate CreateInput issue on schema loading

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -5,6 +5,13 @@ module Mutations
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
 
+    # Fix for duplicate CreateInput error that stops the schema from being loaded
+    # Issue Link: https://github.com/rmosolgo/graphql-ruby/issues/3919
+    def self.default_graphql_name
+      _mutations_namespace, object_name, action_name = self.name.split("::")
+      "#{action_name}#{object_name}"
+    end
+
     def check_authentication!
       return if context[:current_user]
 
