bf2faa6b54cc80c451ec19b99574fab00c934193
BosEriko <bos.eriko@gmail.com>
2023-10-22 17:10:42 +0800

    Feature: Create sample test for User Model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
+# test/models/user_test.rb
+
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user is valid with a name, email, and password" do
+    user = User.new(name: "Naruto Uzumaki", email: "naruto@example.com", password: "password")
+    assert user.valid?
+  end
+
+  test "user is invalid without a name" do
+    user = User.new(email: "sasuke@example.com", password: "password")
+    assert_not user.valid?
+  end
+
+  test "user is invalid without an email" do
+    user = User.new(name: "Sasuke Uchiha", password: "password")
+    assert_not user.valid?
+  end
+
+  test "user is invalid with a duplicate email" do
+    User.create(name: "Sakura Haruno", email: "sakura@example.com", password: "password")
+    user = User.new(name: "Hinata Hyuga", email: "sakura@example.com", password: "password")
+    assert_not user.valid?
+  end
+
+  test "user is valid with a password" do
+    user = User.new(name: "Luffy Monkey D.", email: "luffy@example.com", password: "password")
+    assert user.valid?
+  end
+
+  test "destroys associated posts when destroyed" do
+    user = User.create(name: "Goku Son", email: "goku@example.com", password: "password")
+    post = user.posts.create(content: "Dragon Ball Z")
+    assert_difference('Post.count', -1) do
+      user.destroy
+    end
+  end
 end
