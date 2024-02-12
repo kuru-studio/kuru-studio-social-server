339fbd1b864b64383434bb3c19711c2bbbb1b149
BosEriko <bos.eriko@gmail.com>
2024-02-12 14:42:05 +0800

    remove unique index for user emails

--- db/migrate/20240212062341_remove_email_and_password_digest_from_users.rb
+++ db/migrate/20240212062341_remove_email_and_password_digest_from_users.rb
@@ -2,5 +2,6 @@ class RemoveEmailAndPasswordDigestFromUsers < ActiveRecord::Migration[7.0]
   def change
     remove_column :users, :email
     remove_column :users, :password_digest
+    remove_index :users, :email
   end
 end
