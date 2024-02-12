fee5443ec176cd6782d414f97edd2428fc4ba7c0
BosEriko <bos.eriko@gmail.com>
2024-02-12 14:37:12 +0800

    move migration from removing email and password_digest on a separate migration

--- db/migrate/20240212062342_add_devise_to_users.rb
+++ db/migrate/20240212062342_add_devise_to_users.rb
@@ -37,9 +37,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   end
 
   def self.down
-    change_table :users do |t|
-      t.remove :email
-    end
-    remove_index :users, :email
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
   end
 end
