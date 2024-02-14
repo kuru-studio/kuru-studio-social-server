f79e4224241bc62d04bd5a597978fc14e3f424aa
BosEriko <bos.eriko@gmail.com>
2024-02-14 20:16:05 +0800

    add password_confirmation on type and mutation

--- app/graphql/types/user_attributes.rb
+++ app/graphql/types/user_attributes.rb
@@ -2,5 +2,6 @@ module Types
   class UserAttributes < BaseInputObject
     argument :email, String, required: true
     argument :password, String, required: true
+    argument :password_confirmation, String, required: true
   end
 end
