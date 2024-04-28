0a14bf373103cd81179e0e5366d0e6806507ad60
BosEriko <bos.eriko@gmail.com>
2024-04-28 15:14:29 +0800

    use add_column instead of add_reference

--- db/migrate/20240428070217_add_tenant_id_to_models.rb
+++ db/migrate/20240428070217_add_tenant_id_to_models.rb
@@ -1,7 +1,7 @@
 class AddTenantIdToModels < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :tenant, foreign_key: true, null: false
-    add_reference :posts, :tenant, foreign_key: true, null: false
-    add_reference :users, :tenant, foreign_key: true, null: false
+    add_column :comments, :tenant_id, :uuid, null: false
+    add_column :posts, :tenant_id, :uuid, null: false
+    add_column :users, :tenant_id, :uuid, null: false
   end
 end
