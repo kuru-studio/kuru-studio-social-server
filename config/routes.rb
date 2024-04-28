9868fe82d7a7d3cb75fe2b8208a02164ad4bb7c8
BosEriko <bos.eriko@gmail.com>
2024-04-28 18:04:49 +0800

    return devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Devise related routes
-  # devise_for :users
+  devise_for :users
 
   # Defines the root path route ("/")
   # root "articles#index"
