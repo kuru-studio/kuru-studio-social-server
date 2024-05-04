4ffaad275089fcf154a23d8436d48235563748e4
BosEriko <bos.eriko@gmail.com>
2024-05-05 00:23:32 +0800

    Feature: Simplify Firebase Integration

--- app/services/firebase_verifier_service.rb
+++ app/services/firebase_verifier_service.rb
@@ -10,33 +10,40 @@ class FirebaseVerifierService
     @firebase_project_id = firebase_project_id
   end
 
-  def decode(id_token, public_key)
-    decoded_token, error = decode_jwt_token(id_token, @firebase_project_id, public_key)
-    raise error if error || decoded_token.nil?
+  def decode(id_token)
+    payload, header = decode_jwt_token(id_token, @firebase_project_id)
 
-    payload = decoded_token[0]
-    headers = decoded_token[1]
-    alg = headers['alg']
-
+    alg = header['alg']
     raise "Invalid access token 'alg' header (#{alg}). Must be '#{JWT_ALGORITHM}'." unless alg == JWT_ALGORITHM
 
-    valid_public_keys = retrieve_and_cache_jwt_valid_public_keys
-    kid = headers['kid']
+    typ = header['typ']
+    raise "Invalid access token 'typ' header (#{typ}). Must be 'JWT'." unless typ == 'JWT'
 
+    iss = payload['iss']
+    issuer = "https://securetoken.google.com/#{@firebase_project_id}"
+    raise "Invalid access token 'iss' payload (#{iss}). Must be '#{issuer}'." unless iss == issuer
+
+    exp = payload['exp']
+    raise "Invalid access token 'exp' payload (#{exp}). Token has expired." unless Time.at(exp) > Time.now
+
+    aud = payload['aud']
+    audience = @firebase_project_id
+    raise "Invalid access token 'aud' payload (#{aud}). Must be '#{audience}'." unless aud == audience
+
+    kid = header['kid']
+    valid_public_keys = retrieve_and_cache_jwt_valid_public_keys
     raise "Invalid access token 'kid' header, do not correspond to valid public keys." unless valid_public_keys.keys.include?(kid)
 
     sub = payload['sub']
     raise "Invalid access token. 'Subject' (sub) must be a non-empty string." if sub.nil? || sub.empty?
 
-    exp = payload['exp']
-    raise "Invalid access token. Token has expired." if Time.at(exp) < Time.now
-
-    decoded_token
+    payload
   end
 
   private
 
-    def decode_jwt_token(firebase_jwt_token, firebase_project_id, public_key)
+    def decode_jwt_token(firebase_jwt_token, firebase_project_id)
+      public_key = nil
       custom_options = {
         verify_iat: true,
         verify_aud: true,
@@ -61,7 +68,7 @@ class FirebaseVerifierService
         return nil, "Invalid access token. Signature verification failed."
       end
 
-      return decoded_token, nil
+      return decoded_token
     end
 
     def retrieve_and_cache_jwt_valid_public_keys
