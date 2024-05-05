52cfe40a6be63071e4a8ded788f1971f705b65f4
BosEriko <bos.eriko@gmail.com>
2024-05-05 18:39:03 +0800

    add database level relationship to tenant

--- db/migrate/20240505103314_create_orders.rb
+++ db/migrate/20240505103314_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders, id: :uuid do |t|
+      t.references :tenant, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
     end
--- db/migrate/20240505103320_create_carts.rb
+++ db/migrate/20240505103320_create_carts.rb
@@ -1,6 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts, id: :uuid do |t|
+      t.references :tenant, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
     end
