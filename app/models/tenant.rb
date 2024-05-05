a0902000f2de0f9e6702bc0cfeeb878627a02258
BosEriko <bos.eriko@gmail.com>
2024-05-05 18:43:27 +0800

    add dependent destroy to tenant relationships

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,13 +1,13 @@
 class Tenant < ApplicationRecord
   validates :api_key, uniqueness: true
 
-  has_many :users
-  has_many :posts
-  has_many :comments
-  has_many :carts
-  has_many :inventories
-  has_many :orders
-  has_many :products
+  has_many :users, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :carts, dependent: :destroy
+  has_many :inventories, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :products, dependent: :destroy
 
   %w(shop social forum).each do |feature|
     define_method("is_#{feature}_enabled?") do
