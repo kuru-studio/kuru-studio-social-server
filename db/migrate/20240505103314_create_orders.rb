0077589aa66e73a72c3a696def909dd892e0a500
BosEriko <bos.eriko@gmail.com>
2024-05-07 20:07:46 +0800

    add user_id to orders and cart

--- db/migrate/20240505103314_create_orders.rb
+++ db/migrate/20240505103314_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders, id: :uuid do |t|
-      t.references :tenant, null: false, foreign_key: true, type: :uuid
+      t.uuid :user_id, null: false
+      t.uuid :tenant_id, null: false
 
       t.timestamps
     end
--- db/migrate/20240505103320_create_carts.rb
+++ db/migrate/20240505103320_create_carts.rb
@@ -1,7 +1,8 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts, id: :uuid do |t|
-      t.references :tenant, null: false, foreign_key: true, type: :uuid
+      t.uuid :user_id, null: false
+      t.uuid :tenant_id, null: false
 
       t.timestamps
     end
